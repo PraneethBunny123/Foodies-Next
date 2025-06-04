db969010d8c2d052f2f75cf0cff5d610346f20e2
praneeth <gp.gunturu@gmail.com>
2025-06-04 12:01:28 -0500

    created a dynamic slug page route inside the meals folder.

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Share() {
+    return (
+        <main>
+            <h3>Meals share</h3>
+            <Link href='/meals'>Meals menu</Link>
+        </main>
+    )
+}
