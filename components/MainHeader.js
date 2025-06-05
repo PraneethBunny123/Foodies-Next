b957efb45159f16d067c4be67f0fa8a3a05bf200
praneeth <gp.gunturu@gmail.com>
2025-06-05 15:48:46 -0500

    Creating a header component to use in the root layout

---
+++ components/MainHeader.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import logoImg from '@/assets/logo.png';
+
+export default function MainHeader() {
+    return (
+        <header>
+            <Link href='/'>
+                <img src={logoImg.src} alt="A food plate"/>
+                NextLevel Food
+            </Link>
+        </header>
+    )
+}
