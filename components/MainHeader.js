0e53a05342c8b52d686fcc8445e454bf00f4a0dd
praneeth <gp.gunturu@gmail.com>
2025-06-05 16:06:26 -0500

    Applied styles to the respective tags to get better styling for the header

--- components/MainHeader.js
+++ components/MainHeader.js
@@ -4,7 +4,7 @@ import classes from './main-header.module.css'
 
 export default function MainHeader() {
     return (
-        <header>
+        <header className={classes.header}>
             <Link className={classes.logo} href='/'>
                 <img src={logoImg.src} alt="A food plate"/>
                 NextLevel Food
