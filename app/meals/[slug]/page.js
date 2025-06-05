1c19d0e948e2bb2215c603ecad79115c2a871e48
praneeth <gp.gunturu@gmail.com>
2025-06-05 15:12:37 -0500

    Added links in the main page.js and made few changes

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
-export default function Share({params}) {
+export default function MealDetailsPage({params}) {
     return (
         <main>
-            <h3>Meals {params.slug}</h3>
+            <h3>Meals Details {params.slug}</h3>
             <Link href='/meals'>Meals menu</Link>
         </main>
     )
