44ad8972ca2fc5cb4f730789ab162866f72ad646
praneeth <gp.gunturu@gmail.com>
2025-06-06 16:33:10 -0500

    updated page.js in meals folder

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,12 +1,24 @@
 import Link from "next/link";
+import classes from './page.module.css'
 
 export default function Meals() {
     return (
-        <main>
-            <h1>Meals menu</h1>
-            <p>
-                <Link href='/meals/share'>Share</Link>
-            </p>
-        </main>
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Delicious meals, created
+                    <span className={classes.highlight}>by you</span>
+                </h1>
+                <p className={classes.cta}>
+                    <Link href='/meals/share'>
+                        Share your favorite recipe
+                    </Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+
+            </main>        
+        </>
+
     )
 }
