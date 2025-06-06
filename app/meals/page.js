a084acf187733ce5d87ccf1c2424e93fd15d9329
praneeth <gp.gunturu@gmail.com>
2025-06-06 17:47:16 -0500

    imported that MealGrid file to the page.js in the meals folder under the main tag

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import classes from './page.module.css'
+import MealGrid from "@/components/meals/meals-grid";
 
 export default function Meals() {
     return (
@@ -16,7 +17,7 @@ export default function Meals() {
                 </p>
             </header>
             <main className={classes.main}>
-
+                <MealGrid meals={[]}/>
             </main>        
         </>
 
