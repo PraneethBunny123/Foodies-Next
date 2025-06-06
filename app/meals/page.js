e8aac49fcd4cdd0b440cb97b4d490f87b925f8c6
praneeth <gp.gunturu@gmail.com>
2025-06-06 18:43:33 -0500

    Making the getMeals function async to added a delay for this app so as to use the await, even though we donot need to make this function async for its core functionality. Also making the Meals component in the meals/page.js async to get the meals from the getMeals in async way

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,11 @@
 import Link from "next/link";
 import classes from './page.module.css'
 import MealGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-export default function Meals() {
+export default async function Meals() {
+    const meals = await getMeals()
+
     return (
         <>
             <header className={classes.header}>
