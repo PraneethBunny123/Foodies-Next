4aff64c6069b3741f2e5e815e936648c5595d41f
praneeth <gp.gunturu@gmail.com>
2025-06-05 19:10:58 -0500

    using usePathName hook to get the path of the route. by using that highlighting the browse meals or community based on the page that we are currently on.

--- components/main-header/MainHeader.js
+++ components/main-header/MainHeader.js
@@ -1,9 +1,14 @@
+'use client'
+
 import Link from "next/link";
 import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css'
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 export default function MainHeader() {
+    const path = usePathname()
+
     return (
         <header className={classes.header}>
             <Link className={classes.logo} href='/'>
@@ -14,10 +19,20 @@ export default function MainHeader() {
             <nav className={classes.nav}>
                 <ul>
                     <li>
-                        <Link href='/meals'>Browse Meals</Link>
+                        <Link 
+                            href='/meals' 
+                            className={path.startsWith('/meals') ? classes.active : undefined}
+                        >
+                            Browse Meals
+                        </Link>
                     </li>
                     <li>
-                        <Link href='/community'>Foodies Community</Link>
+                        <Link 
+                            href='/community'
+                            className={path.startsWith('/community') ? classes.active : undefined}
+                        >
+                            Foodies Community
+                        </Link>
                     </li>                    
                 </ul>
             </nav>
