5b277536832d44b8a407b8504b388e58ad201fbe
praneeth <gp.gunturu@gmail.com>
2025-06-07 14:49:19 -0500

    creating a new not-found page inside the meals folder to invoke this custome page if meal is not found. This can happen if someone searches a meal that is not present in the search bar

---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function MealNotFound() {
+    return (
+        <main className="not-found">
+            <h1>Meal Not found</h1>
+            <p>Unfortunately, the page or the meal data you requested is not found</p>
+        </main>
+    )
+}
