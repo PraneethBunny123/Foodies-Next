bed2db4438e293dc61110cde472359f9c8db2cbc
praneeth <gp.gunturu@gmail.com>
2025-06-06 23:43:34 -0500

    Creating a not-found page(not-found is also a reserved keyword) that will display the content we render in that page if the page path is not found

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <main className="not-found">
+            <h1>Not found</h1>
+            <p>Unfortunately, the page you requested is not found</p>
+        </main>
+    )
+}
